72cc9c0d958be1ee884fdeb91d893d7a75354f90
mdmehedyhassan <mdmehedyh55@gmail.com>
2021-04-20 13:19:18 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const fileUpload = require('express-fileupload')
+const MongoClient = require('mongodb').MongoClient;
+require('dotenv').config()
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tnvy9.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use(express.static('admins'));
+app.use(fileUpload())
+
+const port = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.send("hello from db it's working")
+})
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const servicesCollection = client.db("heroBicycle").collection("products");
+  app.post('/services', (req, res) => {
+      const services = req.body;
+      servicesCollection.insertOne(services)
+      .then(result => {
+        res.send(result.insertedCount)
+      })
+  })
+
+  app.post('/addAdmin', (req, res)=> {
+    const file = req.files.file;
+    const name = req.body.name;
+    const email = req.body.email;
+    console.log(name, email, file);
+    file.mv(`${__dirname}/admins/${file.name}`, err => {
+      if(err) {
+        console.log(err);
+        return res.status(500).send({msg: 'Failed to upload image'})
+      }
+      return res.send({name: file.name, path: `/${file.name}`})
+    })
+  })
+
+
+});
+
+app.listen(port)
